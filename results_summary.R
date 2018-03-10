45dad9692a6b94c623c2520717b4373b8e451008
Tristan <tristan.garry@mail.utoronto.ca>
2018-03-10 17:54:46 -0500

    Fixed composition

--- results_summary.R
+++ results_summary.R
@@ -11,10 +11,10 @@ summary_stats <- results %>%
 ## avg and sd of species composition
 composition <- rep_samples %>% 
   rowwise() %>%
-  transform(total = rowSums(rep_samples[8:19]))
-composition[8:19] <- composition[8:19]/composition[21][row(composition[8:19])]
-composition <- composition[-(20:21)]
-composition <- composition %>% 
+  transform(total = rowSums(rep_samples[12:20]))
+composition[12:20] <- composition[12:20]/composition[21][row(composition[12:20])]
+composition <- composition[,-21]; composition <- composition[,-(8:11)]
+composition_local <- composition %>% 
   group_by(.dots=c("connectivity","quality","time")) %>% 
   summarise_at(vars(colnames(composition[,-(1:7)])),funs(mean(.,na.rm=TRUE),sd(.,na.rm=TRUE)))
 
