a39e266a9d1f078a858477307079fffb7ba542f6
Tristan <tristan.garry@mail.utoronto.ca>
2018-03-11 23:34:51 -0400

    Added graphing functions

--- results_summary.R
+++ results_summary.R
@@ -1,3 +1,4 @@
+
 ## averages and sd
 library(dplyr)
 is.na(results)<-sapply(results, is.infinite)
@@ -18,3 +19,16 @@ composition_local <- composition %>%
   group_by(.dots=c("connectivity","quality","time")) %>% 
   summarise_at(vars(colnames(composition[,-(1:7)])),funs(mean(.,na.rm=TRUE),sd(.,na.rm=TRUE)))
 
+
+for (c in 1:length(connec)){
+  for (q in ss.prop){
+    png(paste0("composition_graphs/composition_box_",connec[c] ,q ,".png"))
+    boxplot(x = composition[composition$connectivity==c & composition$quality==q & composition$time == 1000,-(1:7)]
+            ,xlab="species",ylab="composition",outline=FALSE)
+    dev.off()
+    png(paste0("composition_graphs/composition_bar_",c ,q ,".png"))
+    barplot()
+      
+    dev.off()
+  }
+}
