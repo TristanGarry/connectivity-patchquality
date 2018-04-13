65f634af054240af6a88b849b5696c46b03304d8
Tristan <tristan.garry@mail.utoronto.ca>
2018-04-12 22:09:56 -0400

    tentative fix to effect size

--- metacommunity_effsize.R
+++ metacommunity_effsize.R
@@ -1,23 +1,86 @@
 ## effect size & coefficient of variation
 
 # effect size
-library(effsize);library(vegan);library(dplyr);library(ggplot2)
+library(effsize);library(vegan);library(dplyr);library(ggplot2);detach(package:plyr)
+
+# rep_samples <- na.omit(rep_samples) # clean from NAs
+# tran_abundance <- rep_samples[c(1,4:5,8:9,15:23)] # columns we want
+# tran_abundance <- aggregate(tran_abundance[6:14], by = tran_abundance[(c(1:2,4:5))], sum) # effect size is 
+#                                                                         #by metacommunity rather than patch
+# tran_abundance[-(1:4)] <- decostand(tran_abundance[-(1:4)], "hellinger", na.rm = TRUE) # standardise
+# pca_all <- prcomp(tran_abundance[-(1:4)]) #PCA
+# df.pca <- as.data.frame(pca_all$x)
+# df.pca <- df.pca[1:2] # only PC1 & PC2
+# df.pca[3:6] <- tran_abundance[1:4] # identifiers
+# df.pca <- df.pca %>% # find centroids
+#   group_by(time, connectivity, quality) %>%
+#   mutate(centr_PC1 = mean(PC1), centr_PC2 = mean(PC2))
+# df.pca <- df.pca %>% # find distance
+#   mutate(dist = sqrt((PC1 - centr_PC1)^2 + (PC2 - centr_PC2)^2))
+# #df.pca <- df.pca[df.pca$time == 500,]
+
+
+### patchwise differences 
 
 rep_samples <- na.omit(rep_samples) # clean from NAs
+rep_samples <- unique(rep_samples)
 tran_abundance <- rep_samples[c(1,4:5,8:9,15:23)] # columns we want
-tran_abundance <- aggregate(tran_abundance[6:14], by = tran_abundance[(c(1:2,4:5))], sum) # effect size is 
-                                                                        #by metacommunity rather than patch
-tran_abundance[-(1:4)] <- decostand(tran_abundance[-(1:4)], "hellinger", na.rm = TRUE) # standardise
-pca_all <- prcomp(tran_abundance[-(1:4)]) #PCA
+tran_abundance[-(1:5)] <- decostand(tran_abundance[-(1:5)], "hellinger", na.rm = TRUE) # standardise
+pca_all <- prcomp(tran_abundance[-(1:5)]) #PCA
 df.pca <- as.data.frame(pca_all$x)
 df.pca <- df.pca[1:2] # only PC1 & PC2
-df.pca[3:6] <- tran_abundance[1:4] # identifiers
+df.pca[3:7] <- tran_abundance[1:5] # identifiers
 df.pca <- df.pca %>% # find centroids
   group_by(time, connectivity, quality) %>%
   mutate(centr_PC1 = mean(PC1), centr_PC2 = mean(PC2))
 df.pca <- df.pca %>% # find distance
-  mutate(dist = sqrt((PC1 - centr_PC1)^2 + (PC2 - centr_PC2)^2))
+  mutate(dist = sqrt((PC1 - centr_PC1)^2 + (PC2 - centr_PC2)^2)) ## METHOD 1
 #df.pca <- df.pca[df.pca$time == 500,]
+df.pca$test1 <- rep(df.pca[df.pca$connectivity==1 & df.pca$quality==0,]$centr_PC1, times = 24)
+df.pca$test2 <- rep(df.pca[df.pca$connectivity==1 & df.pca$quality==0,]$centr_PC2, times = 24)
+df.pca <- df.pca %>% # find distance
+  mutate(dist2 = sqrt((PC1 - test1)^2 + (PC2 - test2)^2)) ## METHOD 2
+
+
+dists <- data.frame(matrix(NA,nrow=18,ncol=5))
+colnames(dists) <- c("treatment","estimate","low","high","magnitude")
+id <- 1
+for (ss in ss.prop){
+  for(conn in 2:4){
+    d = as.numeric(cbind(df.pca[df.pca$connectivity == conn & df.pca$quality == ss,]$dist2,
+                         df.pca[df.pca$connectivity == 1 & df.pca$quality == 0,]$dist2))
+    f = rep(c("Treatment","Control"), each = 550)
+    co <- cohen.d(d ~ f, paired = TRUE)
+    dists[id,1] <- paste0(connec[conn],ss)
+    dists[id,2] <- co$estimate
+    dists[id,3:4] <- co$conf.int
+    dists[id,5] <- co$magnitude
+    id = id + 1
+  }
+}
+
+#is.na(dists)<-sapply(dists, is.nan); dists[is.na(dists)]<-0; dists[dists$magnitude==0,]$magnitude <- 1
+dists[2:5] <- sapply(dists[2:5], as.numeric)
+for (conn in 2:4){
+  pdf(paste0("rawplots/effsize/",connec[conn] ,"test.pdf"))
+  dat <- dists[grep(paste0(connec[conn]),x=dists$treatment),]
+  plot(0:5, dat$estimate, ylim=range(c(dat$low, dat$high)),
+       pch=19, xlab="patch quality", ylab="effect size",
+       main=paste0(connec[conn]))
+  arrows(0:5, dat$low, 0:5, dat$high,length=0)
+  for (p in 0:5){
+    if (dat[p+1,]$magnitude==2){points(p + 0.2, dat[p+1,]$estimate,pch=3)}
+    else if (dat[p+1,]$magnitude==3){points(p + 0.2, dat[p+1,]$estimate,pch=4)}
+    else if (dat[p+1,]$magnitude==4){points(p + 0.2, dat[p+1,]$estimate,pch=8)}
+  }
+  abline(h=0, lty=2, col = "gray30")
+  dev.off()
+}
+
+
+
+
+
 
 # dists <- data.frame(matrix(NA,nrow=18,ncol=5))
 # colnames(dists) <- c("treatment","estimate","low","high","magnitude")
@@ -68,44 +131,44 @@ df.pca <- df.pca %>% # find distance
 
 ## TRY AGAIN
 
-dists <- data.frame(matrix(NA,nrow=18,ncol=5))
-colnames(dists) <- c("treatment","estimate","low","high","magnitude")
-id <- 1
-for (ss in ss.prop){
-  for(conn in 2:4){
-    d = as.numeric(cbind(df.pca[df.pca$connectivity == conn & df.pca$quality == ss,]$dist,
-                         df.pca[df.pca$connectivity == 1 & df.pca$quality == 0,]$dist))
-    f = rep(c("Treatment","Control"), each = 110)
-    co <- cohen.d(d ~ f, paired = TRUE)
-    dists[id,1] <- paste0(connec[conn],ss)
-    dists[id,2] <- co$estimate
-    dists[id,3:4] <- co$conf.int
-    dists[id,5] <- co$magnitude
-    id = id + 1
-  }
-}
+# dists <- data.frame(matrix(NA,nrow=18,ncol=5))
+# colnames(dists) <- c("treatment","estimate","low","high","magnitude")
+# id <- 1
+# for (ss in ss.prop){
+#   for(conn in 2:4){
+#     d = as.numeric(cbind(df.pca[df.pca$connectivity == conn & df.pca$quality == ss,]$dist,
+#                          df.pca[df.pca$connectivity == 1 & df.pca$quality == 0,]$dist))
+#     f = rep(c("Treatment","Control"), each = 110)
+#     co <- cohen.d(d ~ f, paired = TRUE)
+#     dists[id,1] <- paste0(connec[conn],ss)
+#     dists[id,2] <- co$estimate
+#     dists[id,3:4] <- co$conf.int
+#     dists[id,5] <- co$magnitude
+#     id = id + 1
+#   }
+# }
 
 
 
 
 ## plotting is for timestep 500 only - ignore for now
 
-is.na(dists)<-sapply(dists, is.nan); dists[is.na(dists)]<-0; dists[dists$magnitude==0,]$magnitude <- 1
-dists[2:5] <- sapply(dists[2:5], as.numeric)
-for (conn in 2:4){
-  pdf(paste0("rawplots/effsize/",connec[conn] ,"test2.pdf"))
-  dat <- dists[grep(paste0(connec[conn]),x=dists$treatment),]
-  plot(0:5, dat$estimate, ylim=range(c(dat$low, dat$high)),
-       pch=19, xlab="patch quality", ylab="effect size",
-       main=paste0(connec[conn]))
-  arrows(0:5, dat$low, 0:5, dat$high,length=0)
-  for (p in 0:5){
-    if (dat[p+1,]$magnitude==2){points(p + 0.2, dat[p+1,]$estimate,pch=3)}
-    else if (dat[p+1,]$magnitude==3){points(p + 0.2, dat[p+1,]$estimate,pch=4)}
-    else if (dat[p+1,]$magnitude==4){points(p + 0.2, dat[p+1,]$estimate,pch=8)}
-  }
-  abline(h=0, lty=2, col = "gray30")
-  dev.off()
-}
+# is.na(dists)<-sapply(dists, is.nan); dists[is.na(dists)]<-0; dists[dists$magnitude==0,]$magnitude <- 1
+# dists[2:5] <- sapply(dists[2:5], as.numeric)
+# for (conn in 2:4){
+#   pdf(paste0("rawplots/effsize/",connec[conn] ,"test2.pdf"))
+#   dat <- dists[grep(paste0(connec[conn]),x=dists$treatment),]
+#   plot(0:5, dat$estimate, ylim=range(c(dat$low, dat$high)),
+#        pch=19, xlab="patch quality", ylab="effect size",
+#        main=paste0(connec[conn]))
+#   arrows(0:5, dat$low, 0:5, dat$high,length=0)
+#   for (p in 0:5){
+#     if (dat[p+1,]$magnitude==2){points(p + 0.2, dat[p+1,]$estimate,pch=3)}
+#     else if (dat[p+1,]$magnitude==3){points(p + 0.2, dat[p+1,]$estimate,pch=4)}
+#     else if (dat[p+1,]$magnitude==4){points(p + 0.2, dat[p+1,]$estimate,pch=8)}
+#   }
+#   abline(h=0, lty=2, col = "gray30")
+#   dev.off()
+# }
 
 
